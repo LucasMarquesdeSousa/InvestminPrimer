4e6bc0586cecf04afecab47c72211d6470a6ba89
LucasMarquesdeSousa <marqueslukas66@gmail.com>
2020-12-26 20:11:03 -0300

    master

--- Conexao/Conexao.php
+++ Conexao/Conexao.php
@@ -5,7 +5,7 @@ abstract class Conexao{
         try {
             if(!isset(self::$conectado)){
                 self::$conectado = new PDO("mysql:host=localhost;dbname=hackathon;charset=UTF8", "root", "");
-                self::$conectado->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                     self::$conectado->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             return self::$conectado;
         } catch (PDOException $ex) {
